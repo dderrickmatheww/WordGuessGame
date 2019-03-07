50ce78c38926cf0ed9f8b6f0494743553f80c746
Derrick <dderrickmatheww@gmail.com>
2019-03-07 15:55:26 -0500

    updated verison

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,19 +47,87 @@ function newGame() {
     $guessesLeft.textContent = guessesLeft;
     $placeholders.textContent = pickedWordPlaceholderArr.join("");
     $guessedLetters.textContent = incorrectLetterBank;
-
 }
-
-
 
 //letterGuess function, takes in the letter you pressed and see if it's in the selected word
 
+function letterGuess (letter) {
+    console.log(letter);
+    if (gameRunning === true && guessedLetterBank.indexOf(letter) === -1) {
+        //Run game logic
+        guessedLetterBank.push(letter);
+
+        //Check if guessed letter is in my picked word
+        for (var index = 0; index < pickedWord.length; index++) {
+            //Convert both  to lowercase so I can compare them correctly
+            if(pickedWord[index].toLowerCase() === letter.toLowerCase()) {
+                //if a match, swap out that character in that placeholder with the actual letter
+                pickedWordPlaceholderArr[index] = pickedWord[index];
+            }
+        }
+        $placeholders.textContent = pickedWordPlaceholderArr.join("");
+        //pass letter guessed into the checkIncorrect function
+        checkIncorrect(letter);
+    }
+    else {
+        if (gameRunning === false) {
+            alert("The game isn't running, click on the new game button to start over.");
+        }
+        else {
+            alert("You've already guessed this letter, try another one!");
+        }
+    }
+}
 //checkIncorrect(letter)
+function checkIncorrect(letter) {
+    //Check to see if the letter DIDN'T make it into our pickedWordPlaceholderArr (incorrect guess)
+    if 
+    (pickedWordPlaceholderArr.indexOf(letter.toLowerCase()) === -1 && pickedWordPlaceholderArr.indexOf(letter.toUpperCase()) === -1) {
+        //Subtract guesses
+        guessesLeft--;
+        //put letter into IncorrectLetterBank
+        incorrectLetterBank.push(letter);
+        //write new bank of incorrect letters to DOM
+        $guessedLetters.textContent = incorrectLetterBank.join(" ");
+        //Write new amount of guesses to DOM
+        $guessesLeft.textContent = guessesLeft;
+    }
+    checkLose();
+      
+    
+}
 
 //checkLose
-
+function checkLose() {
+    var audio = new Audio('apex.mp3')
+    if (guessesLeft === 0) {
+        losses++;
+        gameRunning = false;
+        $losses.textContent = losses;
+        audio.play();
+        alert("YOU LOSE!")
+    }
+    checkWin()
+}
 //checkWin
+function checkWin() {
+    var audio = new Audio('Apextheme.mp3.mp3');
+
+    if(pickedWord.toLowerCase() === pickedWordPlaceholderArr.join("").toLowerCase()){
+        wins++;
+        gameRunning = false;
+        $wins.textContent = wins;
+        audio.play();
+        alert("YOU WON!")
+    }
+    
+}
 
 //Add event listener for new game button
 $newGameButton.addEventListener("click", newGame);
 // Add onkeyup event to trigger letterGuess
+document.onkeyup = function(event) {
+    console.dir(event);
+    if (event.keyCode >= 65 && event.keyCode <= 90);
+    letterGuess(event.key);
+}
